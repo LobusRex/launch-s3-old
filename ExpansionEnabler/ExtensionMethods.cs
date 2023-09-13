0e1271dd7220e6e5e281fb0acd114ba2074143a0
Viktor Loby <vl222ig@student.lnu.se>
2023-09-13 11:46:16 +0200

    Document and clean the ExpansionEnabler

--- ExpansionEnabler/ExtensionMethods.cs
+++ ExpansionEnabler/ExtensionMethods.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExpansionEnabler
+﻿namespace ExpansionEnabler
 {
 	public static class ExtensionMethods
 	{
+		/// <summary>
+		/// Replaces all occurrences of a sequence of bytes with another
+		/// sequence of bytes.
+		/// </summary>
+		/// <param name="src"></param>
+		/// <param name="pattern">The byte sequence to replace.</param>
+		/// <param name="replacement">The byte sequence to replace with.</param>
+		/// <exception cref="ArgumentException">Thrown if pattern and
+		/// replacement are not the same size.</exception>
 		public static void ReplaceAll(this byte[] src, byte[] pattern, byte[] replacement)
 		{
+			if (pattern.Length != replacement.Length)
+				throw new ArgumentException("Pattern and replacement must be the same size.");
+
 			int[] matches = FindAllMatches(src, pattern);
 
 			foreach (int match in matches)
@@ -22,6 +27,12 @@ namespace ExpansionEnabler
 		// Asked by Anders R https://stackoverflow.com/users/36504/anders-r
 		// Answered by Ing. Gerardo Sánchez https://stackoverflow.com/users/4685116/ing-gerardo-s%c3%a1nchez
 		// Edited by Elmue https://stackoverflow.com/users/1487529/elmue
+		/// <summary>
+		/// Get all occurrences of a sequence of bytes in the array.
+		/// </summary>
+		/// <param name="src"></param>
+		/// <param name="pattern">The sequence of bytes to look for.</param>
+		/// <returns>An array containing the offset for each occurrence.</returns>
 		private static int[] FindAllMatches(this byte[] src, byte[] pattern)
 		{
 			List<int> results = new List<int>();
@@ -46,6 +57,14 @@ namespace ExpansionEnabler
 			return results.ToArray();
 		}
 
+		/// <summary>
+		/// Insert a sequence of bytes into an array at a specific offset.
+		/// </summary>
+		/// <param name="src"></param>
+		/// <param name="insertion">The sequence of bytes to insert.</param>
+		/// <param name="offset">The index where the insertion should start.</param>
+		/// <exception cref="IndexOutOfRangeException">Thrown if the insertion does
+		/// not fit in the array with the provided offset.</exception>
 		private static void Insert(this byte[] src, byte[] insertion, int offset)
 		{
 			for (int i = 0; i < insertion.Length; i++)
--- ExpansionEnabler/Program.cs
+++ ExpansionEnabler/Program.cs
@@ -1,79 +1,87 @@
 ﻿using Common;
-using Microsoft.Win32;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Security.AccessControl;
-using System.Security.Principal;
 using System.Text;
 
 namespace ExpansionEnabler
 {
-	internal class Program
+	internal static class Program
 	{
-		private static readonly string oldTarget = @"Software\Sims";
-		private static readonly string newTarget = @"Software\SimL";
+		private static string OldTarget { get; } = @"Software\Sims";
+		private static string NewTarget { get; } = @"Software\SimL";
 
-		private static bool showOutput = true;
+		private static bool ShowOutput { get; set; } = true;
 
+		/// <summary>
+		/// The entry point of the ExpansionEnabler. Running this program requires
+		/// elevated permissions to modify a Windows registry key and to modify the
+		/// game installation. When EP selection is enabled, a user can select
+		/// Expansion Packs without elevated permissions. However, the user must
+		/// start the game through TS3L.exe to use the selected packs.
+		/// </summary>
+		/// <param name="args"></param>
 		private static void Main(string[] args)
 		{
-			//foreach (string arg in args)
-			//{
-			//	Console.WriteLine(arg);
-			//}
-
 			string task = args[0];
 			string gamePath = args[1];
 			string volume = args.Length > 2 ? args[2] : "";
 
-			showOutput = volume.ToLower() != "quiet";
+			ShowOutput = volume.ToLower() != "quiet";
 
-			Log("");
-			Log("Old Base: " + MachineRegistry.GetFullKey(MachineRegistry.SimsKey, MachineRegistry.BaseGameKey));
-			Log("New Base: " + MachineRegistry.GetFullKey(MachineRegistry.SimLKey, MachineRegistry.BaseGameKey));
-			// TODO: Add logs for the game path...
+			string simsKey = MachineRegistry.SimsKey;
+			string simLKey = MachineRegistry.SimLKey;
+			string baseGameKey = MachineRegistry.BaseGameKey;
+			string oldGameName = GameDirectory.OldGame;
+			string newGameName = GameDirectory.NewGame;
+
+			Log("Old base key:  " + MachineRegistry.GetFullKey(simsKey, baseGameKey));
+			Log("New base key:  " + MachineRegistry.GetFullKey(simLKey, baseGameKey));
+			Log("Old game path: " + Path.Combine(gamePath, oldGameName));
+			Log("New game path: " + Path.Combine(gamePath, newGameName));
 			Log("");
 
 			if (task.ToLower() == "enable")
 			{
 				// Create the SimL registry key.
-				MachineRegistry.CreateOpenKey(MachineRegistry.SimLKey);
-				Log("Created " + MachineRegistry.GetFullKey(MachineRegistry.SimLKey));
+				MachineRegistry.CreateOpenKey(simLKey);
+				Log("Created " + MachineRegistry.GetFullKey(simLKey));
 
 				// Copy the "The Sims 3" subkey from Sims to SimL.
-				MachineRegistry.CopyKey(MachineRegistry.SimsKey, MachineRegistry.SimLKey, MachineRegistry.BaseGameKey);
-				Log("Copied to " + MachineRegistry.GetFullKey(MachineRegistry.SimLKey, MachineRegistry.BaseGameKey));
+				MachineRegistry.CopyKey(simsKey, simLKey, baseGameKey);
+				Log("Copied to " + MachineRegistry.GetFullKey(simLKey, baseGameKey));
 
 				// Create the new game TS3L.exe.
 				CreateTS3L(gamePath);
-				Log("Created " + Path.Combine(gamePath, GameDirectory.NewGame));
+				Log("Created " + Path.Combine(gamePath, newGameName));
 			}
 			else if (task.ToLower() == "disable")
 			{
 				// Delete the new game TS3L.exe.
 				DeleteTS3L(gamePath);
-				Log("Deleted " + Path.Combine(gamePath, GameDirectory.NewGame));
+				Log("Deleted " + Path.Combine(gamePath, newGameName));
 
 				// Delete the new registry key SimL.
-				MachineRegistry.DeleteKey(MachineRegistry.SimLKey);
-				Log("Deleted " + MachineRegistry.GetFullKey(MachineRegistry.SimLKey));
+				MachineRegistry.DeleteKey(simLKey);
+				Log("Deleted " + MachineRegistry.GetFullKey(simLKey));
 			}
 
-			if (showOutput)
+			if (ShowOutput)
 			{
 				Console.ReadLine();
 			}
 		}
 
+		/// <summary>
+		/// Creates a copy of TS3W.exe called TS3L.exe. The copy uses different Windows Registry keys for finding Expansion Packs.
+		/// </summary>
+		/// <param name="gamePath">The path to the parent directory of TS3W.exe.</param>
+		/// <exception cref="Exception">Thrown if anything unexpected occurs.</exception>
 		private static void CreateTS3L(string gamePath)
 		{
 			string newGamePath = Path.Combine(gamePath, GameDirectory.NewGame);
 			string oldGamePath = Path.Combine(gamePath, GameDirectory.OldGame);
 
 			// Translate "Software\Sims" and "Software\SimL to bytes"
-			byte[] oldTargetBytes = Encoding.UTF8.GetBytes(oldTarget);
-			byte[] newTargetBytes = Encoding.UTF8.GetBytes(newTarget);
+			byte[] oldTargetBytes = Encoding.UTF8.GetBytes(OldTarget);
+			byte[] newTargetBytes = Encoding.UTF8.GetBytes(NewTarget);
 
 			// Read TS3W.exe.
 			byte[] newGameContent = File.ReadAllBytes(oldGamePath);
@@ -85,6 +93,11 @@ namespace ExpansionEnabler
 			File.WriteAllBytes(newGamePath, newGameContent);
 		}
 
+		/// <summary>
+		/// Deletes TS3L.exe.
+		/// </summary>
+		/// <param name="gamePath">The path to the parent directory of TS3W.exe.</param>
+		/// <exception cref="Exception">Thrown if anything unexpected occurs.</exception>
 		private static void DeleteTS3L(string gamePath)
 		{
 			// Delete TS3L.exe.
@@ -92,9 +105,13 @@ namespace ExpansionEnabler
 			File.Delete(newGamePath);
 		}
 
+		/// <summary>
+		/// Writes a message to the Console if ShowOutput is true.
+		/// </summary>
+		/// <param name="message">The message to log.</param>
 		private static void Log(string message)
 		{
-			if (showOutput)
+			if (ShowOutput)
 			{
 				Console.WriteLine(message);
 			}
