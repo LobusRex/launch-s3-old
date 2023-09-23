8520919c2704428c71690d4ec367e60d6b1c124e
Viktor Loby <vl222ig@student.lnu.se>
2023-09-23 16:38:13 +0200

    Add parameters to ConvertTo-Ico

--- LobuS3Launcher/Assets/ConvertTo-Ico.ps1
+++ LobuS3Launcher/Assets/ConvertTo-Ico.ps1
@@ -1,21 +1,34 @@
+param(
+	# The path to the Svg to convert.
+	$inputSvg = '.\playbbob.svg',
+
+	# The path to the new Ico file.
+	$outputIco  = '.\playbbob.ico',
+
+	# A place to temporarily store images in.
+	$temp = '.\temp'
+)
+
 # Sizes come from the Icon Scaling table at
 # https://learn.microsoft.com/en-us/windows/apps/design/style/iconography/app-icon-construction
 $sizes = 16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 256
 
-New-Item -ItemType "directory" -Path .\temp -Force
+# Create a place to store Png images.
+New-Item -ItemType 'directory' -Path $temp -Force
 
 # Create Png images.
 Foreach ($size in $sizes)
 {
-	inkscape playbbob.svg --export-filename=temp\$size.png -w $size -h $size
+	inkscape $inputSvg --export-filename="$temp\$size.png" -w $size -h $size
 }
 
 # Create an Ico file from the images.
 $images = @()
 Foreach ($size in $sizes)
 {
-	$images += "temp\\{0}{1}" -f $size,".png"
+	$images += "$temp\$size.png"
 }
-convert $images playbbob.ico
+convert $images $outputIco
 
-Remove-Item -Path .\temp -Recurse
+# Remove the temporary directory.
+Remove-Item -Path $temp -Recurse
